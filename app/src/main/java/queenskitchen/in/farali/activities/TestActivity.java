ab495f0255024f2c837a140e00a964c482609208
ANKIT <adkhatri77@gmail.com>
2016-10-04 01:15:07 +0530

    database sorting

--- app/src/main/java/queenskitchen/in/farali/activities/TestActivity.java
+++ app/src/main/java/queenskitchen/in/farali/activities/TestActivity.java
@@ -8,16 +8,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
 
-import com.squareup.okhttp.MediaType;
-import com.squareup.okhttp.MultipartBuilder;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.RequestBody;
-import com.squareup.okhttp.Response;
 
 import org.apache.commons.lang.StringEscapeUtils;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import queenskitchen.in.farali.R;
@@ -36,49 +31,91 @@ public class TestActivity extends AppCompatActivity {
         String Title = StringEscapeUtils.unescapeJava(unicode);
         tvtest.setText(Title);
         tvtest.setMovementMethod(LinkMovementMethod.getInstance());
-        new AsyncPhoto().execute();
+//        new AsyncPhoto().execute();
     }
 
-    private RequestBody generateUpdateProfileRequest() {
-
-        String FilePath = Environment.getExternalStorageDirectory() + "/test.jpg";
-        final WSConstants wsConstants = new WSConstants();
-        final MultipartBuilder multipartBuilder = new MultipartBuilder();
-        multipartBuilder.type(MultipartBuilder.FORM);
-
-        multipartBuilder.addFormDataPart("event_date", "2006-10-25 14:30:59");
-        multipartBuilder.addFormDataPart("device", "1");
-
-        final File fileAvatarImage = new File(FilePath);
-
-        multipartBuilder.addFormDataPart("file", fileAvatarImage.getName(), RequestBody.create(MediaType.parse("image/png"), fileAvatarImage));
-
-        return multipartBuilder.build();
-    }
-
-    public String callServiceHttpPostBulk() {
-        try {
-            final OkHttpClient okHttpClient = new OkHttpClient();
-            okHttpClient.setConnectTimeout(WSConstants.CONNECTION_TIMEOUT, TimeUnit.SECONDS);
-            okHttpClient.setReadTimeout(WSConstants.CONNECTION_TIMEOUT, TimeUnit.SECONDS);
-            Request request = new Request.Builder()
-                    .url("http://volanze.com/api/screenshotlogs/")
-                    .post(generateUpdateProfileRequest())
-                    .build();
-            Response response = okHttpClient.newCall(request).execute();
-            return response.body().string();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "";
-        }
-
-    }
-
-    private class AsyncPhoto extends AsyncTask<Void, Void, Void> {
-        @Override
-        protected Void doInBackground(Void... params) {
-            callServiceHttpPostBulk();
-            return null;
-        }
-    }
+//    public MultipartBody uploadRequestBody() {
+//
+//        MediaType MEDIA_TYPE = MediaType.parse("image/jpg"); // e.g. "image/png"
+//        return new MultipartBody.Builder()
+//                .setType(MultipartBody.FORM)
+//                .addFormDataPart("event_date", "2006-10-25 14:30:59")
+//                .addFormDataPart("device", "1")
+//                .addFormDataPart("file", "test1.jpg", RequestBody.create(MEDIA_TYPE, new File(Environment.getExternalStorageDirectory() + "/test1.jpg")))
+//                .build();
+//    }
+//
+////    private RequestBody generateUpdateProfileRequest() {
+////
+////        String FilePath = Environment.getExternalStorageDirectory() + "/test1.jpg";
+////        final WSConstants wsConstants = new WSConstants();
+////        final MultipartBuilder multipartBuilder = new MultipartBuilder();
+////        multipartBuilder.type(MultipartBuilder.FORM);
+////
+////        multipartBuilder.addFormDataPart("event_date", "2006-10-25 14:30:59");
+////        multipartBuilder.addFormDataPart("device", "1");
+////
+////        final File fileAvatarImage = new File(FilePath);
+////
+////        multipartBuilder.addFormDataPart("file", fileAvatarImage.getName(), RequestBody.create(MediaType.parse("image/png"), fileAvatarImage));
+////
+////        return multipartBuilder.build();
+////    }
+//
+//    public static String POST(OkHttpClient client, HttpUrl url, RequestBody body) throws IOException {
+//        Request request = new Request.Builder()
+//                .url("http://volanze.com/api/screenshotlogs/")
+//                .post(body)
+//                .build();
+//        Response response = client.newCall(request).execute();
+//        return response.body().string();
+//    }
+//
+//
+//    //    public String callServiceHttpPostBulk() {
+////        try {
+////            String FilePath = Environment.getExternalStorageDirectory() + "/test1.jpg";
+////            final File fileAvatarImage = new File(FilePath);
+////
+////            final OkHttpClient okHttpClient = new OkHttpClient();
+////            okHttpClient.setConnectTimeout(WSConstants.CONNECTION_TIMEOUT, TimeUnit.SECONDS);
+////            okHttpClient.setReadTimeout(WSConstants.CONNECTION_TIMEOUT, TimeUnit.SECONDS);
+////            RequestBody requestBody = generateUpdateProfileRequest();
+////            Request request = new Request.Builder()
+////                    .url("http://volanze.com/api/screenshotlogs/").
+////            .header("Content-Length", "" + requestBody.contentLength())
+////                    .post(generateUpdateProfileRequest())
+////                    .build();
+////            Response response = okHttpClient.newCall(request).execute();
+////            return response.body().string();
+////        } catch (Exception e) {
+////            e.printStackTrace();
+////            return "";
+////        }
+////
+////    }
+//    public static HttpUrl buildURL() {
+//        return new HttpUrl.Builder()
+//                .scheme("http") //http
+//                .host("volanze.com")
+//                .addPathSegment("api/screenshotlogs/")//adds "/pathSegment" at the end of hostname
+//                .build();
+//        /**
+//         * The return URL:
+//         *  https://www.somehostname.com/pathSegment?param1=value1&encodedName=encodedValue
+//         */
+//    }
+//
+//    private class AsyncPhoto extends AsyncTask<Void, Void, Void> {
+//        @Override
+//        protected Void doInBackground(Void... params) {
+//            try {
+//                POST(new OkHttpClient(), buildURL(), uploadRequestBody());
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//
+//            return null;
+//        }
+//    }
 }
