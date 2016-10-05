22a3388c99b48a419eacf291963f9a76e57e70a2
Ankit Khatri <ankit.khatri@indianic.com>
2016-10-05 20:08:22 +0530

    Phase2

--- app/src/main/java/queenskitchen/in/farali/webservice/WSCheckVersion.java
+++ app/src/main/java/queenskitchen/in/farali/webservice/WSCheckVersion.java
@@ -1,18 +1,7 @@
 package queenskitchen.in.farali.webservice;
 
 import android.content.Context;
-import android.text.TextUtils;
 import android.util.Log;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.Calendar;
-
-import queenskitchen.in.farali.common.Const;
-import queenskitchen.in.farali.common.Utils;
-import queenskitchen.in.farali.model.RecipesModel;
 
 /**
  * Created by ANKIT on 10/3/2016.
@@ -37,9 +26,9 @@ public class WSCheckVersion {
         Log.e("WebserviceUrl", url);
         String str = WebService.getMethod(url);
         if (str.equalsIgnoreCase("\"Updated\"")) {
-            return true;
-        } else {
             return false;
+        } else {
+            return true;
         }
     }
 
